030bab2db6dcb554d8b361875795da4d616ba288
mahmoudhossam <mahmoud.modo45@gmail.com>
2024-12-17 00:11:29 +0200

    Adding Show Model Bottom Sheet && Zero Padding in the List View Builder ✅

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/add_note_model_sheet.dart';
 import 'package:notes_app/views/widgets/note_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -9,8 +10,14 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blueAccent,
-        onPressed: () {},
+        backgroundColor: Colors.lightBlue,
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return const AddNoteBottomSheet();
+              });
+        },
         shape: const CircleBorder(), // Enforce circular shape
         child: const Icon(Icons.add),
       ),
